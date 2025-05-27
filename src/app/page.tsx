232b6e4d7e393bf7b6e3a249b94517c542972360
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-26 23:39:52 -0300

    chrore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-// import Image from "next/image";
-
 // página inicial
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="gap-2 text-red-400">
       <h1>Clínica de Odontologia</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
