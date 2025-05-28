5b7d0ec9f483356083f905624668f6e7dd3a6dcb
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-28 17:12:23 -0300

    feat: implement clinic retrieval and redirection based on user association in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import SignOutButton from "./components/sign-out-button";
@@ -14,6 +17,16 @@ const DashboardPage = async () => {
 
   if (!session?.user) {
     redirect("/authentication");
+  }
+
+  // ele deve retornar um array, se o array for vazio, o user não tem clinica
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    // verificamos se o userId que está no banco é o mesmo que o da sessão
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
   }
 
   return (
