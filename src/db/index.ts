3b98b72a3d64764748bdbd5da1eb00df6afeb23a
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-28 17:12:00 -0300

    feat: enhance database connection by integrating schema for auto-completion of entities

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,9 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+import * as schema from "./schema";
+
+// para que possamos ter o auto complite das entidades/tabelas do banco de dados, passamos como segundo argumento o schema
+export const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+});
