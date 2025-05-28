35622dfb7bd45d474672895a8c703a192b84c568
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-27 23:24:07 -0300

    feat: implement authentication and dashboard pages with session handling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,27 @@
-const DashboardPage = () => {
-  return <div>Dashboard</div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+// um server componente ele pode ser assincrono e pode chamar seu banco de dados, por isso o next é um framework fullstack
+const DashboardPage = async () => {
+  // obtendo os dados da sessão
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div>
+      <h1>{session?.user?.name}</h1>
+      <SignOutButton />
+    </div>
+  );
 };
 
 export default DashboardPage;
