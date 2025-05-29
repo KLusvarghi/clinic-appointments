eccd2e09af20fc3ffe1e0d8205cf900d3a78b3d5
KLusvarghi <kauaolusvarghi@gmail.com>
2025-05-29 17:13:42 -0300

    feat: enhance authentication client with custom session handling and integrate clinic data retrieval

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,14 @@
+import { customSessionClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-export const authClient =
-  createAuthClient();
-  // como nossa api é integrada co o next, não tem essa necessidade de passar o baseURL
-  //   {
-  //     baseURL: "http://localhost:3000"
-  // }
+
+import { auth } from "./auth";
+
+// para que ele leve em consideração as propriedades customizadas que criamos no "auth.ts" que fica dentro de "pluguins",
+export const authClient = createAuthClient({
+  plugins: [customSessionClient<typeof auth>()],
+});
+
+// como nossa api é integrada co o next, não tem essa necessidade de passar o baseURL
+//   {
+//     baseURL: "http://localhost:3000"
+// }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,13 @@
+// tudo que é feito aqui, roda ao lado do servidor
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { customSession } from "better-auth/plugins";
+import { eq } from "drizzle-orm";
 
 import { db } from "@/db";
 // pegando todos os schemas que estão sendo exportados lá de "schemas"
 import * as schema from "@/db/schema";
+import { usersToClinicsTable } from "@/db/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -13,11 +17,43 @@ export const auth = betterAuth({
   }),
   // Configuração para autenticação com google:
   socialProviders: {
-    google: { 
-        clientId: process.env.GOOGLE_CLIENT_ID as string, 
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
-    }, 
-},
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  // o better auth, quando se quer fazer algo a mais, ele tem esse sistema de plugins, tendo vários tipos e um deles é o "customSession"
+  // para que possamos modificar o que o "authClient.useSession" retorna, precisamos usar esse "customSession" e customizar a sessão do meu usuário:
+  plugins: [
+    // e ele recebe uma arrowfunction e já desestruturamos as props
+    customSession(async ({ user, session }) => {
+      // e aqui dentro, fazemos nada menos que uma query dentro da tabela de relacioamento "users and clinics"
+      // assim tendo apenas os dados do relacionamento entre o user e a clinica
+      const clinics = await db.query.usersToClinicsTable.findMany({
+        where: eq(usersToClinicsTable.userId, user.id),
+        // E nesse caso precisamos do nome da clinica, para isso, apenas:
+        with: {
+          clinic: true,
+          // user: true, // caso quisessemos os dados do user
+        },
+      });
+      // TODO: No futuro, alterar essa lógica para que retorne todas as clinicas do usuário, não apenas a primeira ocorrencia
+      const clinic = clinics?.[0];
+      return {
+        user: {
+          ...user, // retorna todos os dados do user
+          clinic: clinic?.clinicId
+            ? {
+                id: clinic?.clinicId,
+                name: clinic?.clinic?.name,
+              }
+            : undefined,
+        },
+        session,
+      };
+    }),
+  ],
+
   // lembra do schema que o better-auth criou? enttão, temos que deixar explicito o nome das variáveis que usamos conforme as tabelas:
   user: {
     modelName: "usersTable",
