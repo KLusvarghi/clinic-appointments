ef8097248f7a8d2ccfdbe33705e437c8d2bf2681
KLusvarghi <kauaolusvarghi@gmail.com>
2025-06-02 22:47:09 -0300

    feat: implement deleteAppointment action with authorization checks and revalidation for appointment deletion

---
+++ src/actions/delete-appointment/index.tsx
@@ -0,0 +1,46 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
+import { headers } from "next/headers";
+import { z } from "zod";
+
+import { db } from "@/db";
+import { appointmentsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+import { actionClient } from "@/lib/safe-action";
+
+export const deleteAppointment = actionClient
+  .schema(
+    z.object({
+      id: z.string().uuid(),
+    }),
+  )
+  .action(async ({ parsedInput }) => {
+    const session = await auth.api.getSession({
+      headers: await headers(),
+    });
+
+    if (!session?.user) {
+      throw new Error("Unauthorized");
+    }
+
+    const appointment = await db.query.appointmentsTable.findFirst({
+      where: eq(appointmentsTable.id, parsedInput.id),
+    });
+
+    if (!appointment) {
+      throw new Error("Appointment doesn't exist");
+    }
+
+    if (appointment.clinicId !== session.user.clinic?.id) {
+      throw new Error("Appointment doesn't belong to the clinic");
+    }
+
+    await db
+      .delete(appointmentsTable)
+      .where(eq(appointmentsTable.id, parsedInput.id));
+    revalidatePath("/appointments");
+
+    return { success: true };
+  });
