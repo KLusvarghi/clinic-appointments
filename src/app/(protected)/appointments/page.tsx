74b448a21dccb1d0dbee59daa844b64bf5461a0d
KLusvarghi <kauaolusvarghi@gmail.com>
2025-06-02 22:47:40 -0300

    feat: create AppointmentsPage component for managing clinic appointments with data fetching and user authentication

---
+++ src/app/(protected)/appointments/page.tsx
@@ -0,0 +1,72 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { DataTable } from "@/components/ui/data-table";
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+import { db } from "@/db";
+import { appointmentsTable, doctorsTable, patientsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import AddAppointmentButton from "./_components/add-appointment-button";
+import { AppointmentsTableColumns } from "./_components/table-column";
+
+const AppointmentsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+
+  // o "promise.all" é usado para executar as consultas em paralelo, então todas essas querys no banco de dados serão executadas ao mesmo tempo, nos dando mais performance
+  const [patients, doctors, appointments] = await Promise.all([
+    db.query.patientsTable.findMany({
+      where: eq(patientsTable.clinicId, session.user.clinic.id),
+    }),
+    db.query.doctorsTable.findMany({
+      where: eq(doctorsTable.clinicId, session.user.clinic.id),
+    }),
+    db.query.appointmentsTable.findMany({
+      where: eq(appointmentsTable.clinicId, session.user.clinic.id),
+      with: {
+        patient: true,
+        doctor: true,
+      },
+    }),
+  ]);
+
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Agendamentos</PageTitle>
+          <PageDescription>
+            Gerencie os agendamentos da sua clínica
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <AddAppointmentButton doctors={doctors} patients={patients} />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <DataTable data={appointments} columns={AppointmentsTableColumns} />
+      </PageContent>
+    </PageContainer>
+  );
+};
+
+export default AppointmentsPage;
