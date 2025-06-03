0cae665b731a2bbc6d8dad9e3b6e0b23ce71d233
KLusvarghi <kauaolusvarghi@gmail.com>
2025-06-02 22:46:54 -0300

    feat: add validation schema for adding appointments using Zod

---
+++ src/actions/add-appointment/schema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const addAppointmentSchema = z.object({
+  patientId: z.string().uuid({
+    message: "Pacient is required.",
+  }),
+  doctorId: z.string().uuid({
+    message: "Doctor is required.",
+  }),
+  date: z.date({
+    message: "Date is required.",
+  }),
+  time: z.string().min(1, {
+    message: "Time is required.",
+  }),
+  appointmentPriceInCents: z.number().min(1, {
+    message: "Appointment price is required.",
+  }),
+});
