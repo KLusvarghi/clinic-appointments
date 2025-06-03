db5fca51d274b9f2fef921eca75038c3c8224914
KLusvarghi <kauaolusvarghi@gmail.com>
2025-06-02 22:47:54 -0300

    feat: add generateTimeSlots function to create time slots between 05:00 and 23:00 at 30-minute intervals

---
+++ src/helpers/time.ts
@@ -0,0 +1,11 @@
+// essa função irá gerar os "slots" de horários entre [05:00:00] e [23:00:00] com intervalo de 30 minutos
+export const generateTimeSlots = () => {
+  const slots = [];
+  for (let hour = 5; hour <= 23; hour++) {
+    for (let minute = 0; minute < 60; minute += 30) {
+      const timeString = `${hour.toString().padStart(2, "0")}:${minute.toString().padStart(2, "0")}:00`;
+      slots.push(timeString);
+    }
+  }
+  return slots;
+};
